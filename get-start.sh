1975a395ce2ae50544b3580c55d43b19a0adb0d5
fudali <fudali113@gmail.com>
2018-03-22 11:59:10 +0800

    fix: 修复getstart里面的设置GOPATH

--- get-start.sh
+++ get-start.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 git clone https://git.23cube.com/tools/mongosync mongosync/src/github/fudali113/mongosync
 
-export GOPATH=$pwd/mongosync:$GOPATH
+export GOPATH=$PWD/mongosync:$GOPATH
 cd mongosync/src/github/fudali113/mongosync
